66c361abefdc38e6ed9e6f5b751cde1e8c3bccfb
Alex Chojnacki (the-alex) <thealex@umich.edu>
2015-11-18 02:20:28 -0500

    Thumbnailed all the images for banner and native on front pages. Should fix loading speed.

---
+++ current/native/c/images/res.py
@@ -0,0 +1,17 @@
+import os, sys
+from PIL import Image
+
+size = 128, 128
+
+
+files = os.listdir('./')
+
+for infile in files:
+    outfile = os.path.splitext(infile)[0] + ".thumbnail"
+    if infile != outfile:
+        try:
+            im = Image.open(infile)
+            im.thumbnail(size, Image.ANTIALIAS)
+            im.save(outfile, "JPEG")
+        except IOError:
+            print "cannot create thumbnail for '%s'" % infile
